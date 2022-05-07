4460d9629f3457482b9648e88f507c5384759706
four_WW <3236961631@qq.com>
2022-05-07 23:31:49 +0800

    Update Config.py

--- CMM/Config.py
+++ CMM/Config.py
@@ -5,7 +5,7 @@ class Config:
     HISTORY_MAX_LENGTH = 50
     FEATURE_MAX_LENGTH = 15
 
-    HISTORY_MIN_LENGTH = 200
+    HISTORY_MIN_LENGTH = 0
     nsample = 4
     EPOCHS = 10
     BATCH_SIZE = 32
@@ -15,14 +15,14 @@ class Config:
     DROUPT = 0.2
 
     DATASET_TYPE = "small"
-    PRINT_LENGHT = 2000
+    PRINT_LENGHT = 1000
 
     BEHAVIORS_NAME = ["ImpressionID", "UserID", "Time", "History", "Impressions"]
     NEWS_NAME = ["NewsID", "Category", "SubCategory", "Title", "Abstract", "Body_url", "TitleEntities",
                  "AbstractEntities"]
 
     PROCESS_DATA_PATH = "../data/processedData/"
-    GLOVE_PATH = '../data/glove/glove.426B.300d.txt'
+    GLOVE_PATH = '../data/glove/glove.42B.300d.txt'
     FEATURE_EMB_LENGTH = 300
     Q, K, V = 300, 512, 512
     HIDDEN_SIZE = 512
